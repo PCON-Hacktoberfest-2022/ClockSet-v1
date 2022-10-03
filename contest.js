704e1b4e573aeb8af0cabb0c352eec490e602013
Shubham Goyal <22shubh22@gmail.com>
2022-10-03 12:24:39 +0530

    fix: datetime to indian time format

--- contest.js
+++ contest.js
@@ -8,8 +8,8 @@ fetch("https://www.kontests.net/api/v1/leet_code")
     compledata.reverse().map((values) => {
       data1 += `<div id="all_contest"><div class="c_name">${values.name}</div>
         <div class="btn btn-danger"><a href="${values.url}" target="_blank">Contest Link</a></div>
-        <div class="start_time">${values.start_time}</div>
-        <div class="end_time">${values.end_time}</div></div>`;
+        <div class="start_time">${datetime_to_indian_format(values.start_time)}</div>
+        <div class="end_time">${datetime_to_indian_format(values.end_time)}</div></div>`;
       // let x = `${values.url}`;
       // if (x.includes("codechef")) {
       //   console.log("Yes");
@@ -17,9 +17,27 @@ fetch("https://www.kontests.net/api/v1/leet_code")
       //   codeforces_e.style["backgroundColor"] = "red";
       // }
     });
-
     document.getElementById("contests").innerHTML = data1;
   });
 // .catch.log((err) => {
 //   console.log(err);
 // });
+function datetime_to_indian_format(datetime){
+  date = new Date(datetime)
+  day = date.getDate()
+  month = date.getMonth()
+  year = date.getFullYear()
+  hour = date.getHours()
+  min = date.getMinutes().toLocaleString('en-US', {
+    minimumIntegerDigits: 2,
+    useGrouping: false
+  })
+  is_am = true
+  if (hour > 12){
+    hour = hour - 12
+  }
+  else if (hour == 0) {
+    hour = 12
+  }
+  return day.toString().concat("/", month.toString(), "/", year.toString(), " ", hour.toString(), ":", min.toString(), " ", (is_am) ? 'AM' : 'PM')
+}
