4e7eea13941df2344c6cf2a976b2bfd8fc33cec9
Soumik Dutta <shalearkane@gmail.com>
2022-10-05 11:08:54 +0530

    add code to fetch contest details from leetcode and atcoder
    Signed-off-by: Soumik Dutta <shalearkane@gmail.com>

--- contest.js
+++ contest.js
@@ -1,5 +1,5 @@
 fetch("https://www.kontests.net/api/v1/leet_code")
-// fetch("https://www.kontests.net/api/v1/all")
+  // fetch("https://www.kontests.net/api/v1/all")
   .then((data) => {
     return data.json();
   })
@@ -19,10 +19,40 @@ fetch("https://www.kontests.net/api/v1/leet_code")
     });
     document.getElementById("contests").innerHTML = data1;
   });
+
+fetch("https://www.kontests.net/api/v1/codeforces")
+  .then((data) => {
+    return data.json();
+  })
+  .then((compledata) => {
+    let data1 = "";
+    compledata.reverse().map((values) => {
+      data1 += `<div id="all_contest"><div class="c_name">${values.name}</div>
+          <div class="btn btn-danger"><a href="${values.url}" target="_blank">Contest Link</a></div><br/>
+          <div class="start_time text-light"><b class="text-light">Start Time: </b>${datetime_to_indian_format(values.start_time)}</div><br/>
+          <div class="end_time text-light"><b class="\text-light">End Time: </b>${datetime_to_indian_format(values.end_time)}</div></div>`;
+    });
+    document.getElementById("contests_codeforces").innerHTML = data1;
+  });
+
+fetch("https://www.kontests.net/api/v1/at_coder")
+  .then((data) => {
+    return data.json();
+  })
+  .then((compledata) => {
+    let data1 = "";
+    compledata.reverse().map((values) => {
+      data1 += `<div id="all_contest"><div class="c_name">${values.name}</div>
+            <div class="btn btn-danger"><a href="${values.url}" target="_blank">Contest Link</a></div><br/>
+            <div class="start_time text-light"><b class="text-light">Start Time: </b>${datetime_to_indian_format(values.start_time)}</div><br/>
+            <div class="end_time text-light"><b class="\text-light">End Time: </b>${datetime_to_indian_format(values.end_time)}</div></div>`;
+    });
+    document.getElementById("contests_at_coder").innerHTML = data1;
+  });
 // .catch.log((err) => {
 //   console.log(err);
 // });
-function datetime_to_indian_format(datetime){
+function datetime_to_indian_format(datetime) {
   date = new Date(datetime)
   day = date.getDate()
   month = date.getMonth()
@@ -33,7 +63,7 @@ function datetime_to_indian_format(datetime){
     useGrouping: false
   })
   is_am = true
-  if (hour > 12){
+  if (hour > 12) {
     hour = hour - 12
   }
   else if (hour == 0) {
