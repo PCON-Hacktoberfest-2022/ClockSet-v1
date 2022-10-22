4552288bf881402cf0219f73ea96a200beeb7fff
Shubham Goyal <22shubh22@gmail.com>
2022-10-22 12:52:04 +0530

    feat: writing in dom

--- contest.js
+++ contest.js
@@ -51,3 +51,41 @@ fetch("https://www.kontests.net/api/v1/codeforces")
     }
   }
 );
+
+fetch("https://www.kontests.net/api/v1/codeforces")
+  .then((data) => {
+    return data.json();
+  })
+  .then((completedata) => {
+    for(let i=0;i<completedata.length;i++){
+      let card = document.createElement("div");
+      // TODO: img in card
+      var fragment = document.createDocumentFragment();
+      let title = document.createElement("H5");
+      title.innerHTML = 'title:' + completedata[i].name
+      fragment.appendChild(title);
+      let duration = document.createElement("P");
+      duration.innerHTML = "Duration:".bold() + completedata[i].duration/60+completedata[i].duration%60;
+      fragment.appendChild(duration);
+      let start_time = document.createElement("P");
+      start_time.innerHTML = "Event starts at:".bold() + moment(completedata[i].start_time).format('LLLL');
+      fragment.appendChild(start_time);
+      let end_time = document.createElement("P");
+      end_time.innerHTML = "Event ends at:".bold() + moment(completedata[i].end_time).format('LLLL');
+      fragment.appendChild(end_time);
+      let time_left = document.createElement("P");
+      time_left.innerHTML = "Event starts ".bold() + moment(completedata[i].start_time).fromNow();
+      fragment.appendChild(time_left);
+      let link_text = document.createTextNode("This is link");
+      let link = document.createElement("A");
+      link.appendChild(link_text);
+      console.log(completedata[i].url)
+      link.href = completedata[i].url;
+      link.title = "Link"
+      fragment.appendChild(link);
+      card.appendChild(fragment);
+      let container = document.querySelector("#container");
+      container.appendChild(card);
+    }
+  }
+);
